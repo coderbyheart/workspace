403abd19f990b68ce499cc01fef70f3300bd6a07
Markus Tacker <markus.tacker@fintura.de>
2015-08-27 11:46:52 +0200

    Add ignore file.

--- inotifyrun.sh
+++ inotifyrun.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-FORMAT=$(echo -e "\033[1;33m%w%f\033[0m written")
+
+# You can write a file which contains path names to exclude in the current directory
+# called .inotifyignore
+#
+# It can contain path matchers, one per line, e.g.
+#
+# .git/*
+# tmp/*
+
+FORMAT=$(echo -e "%w%f written")
+EXCLUDE=`cat .inotifyignore | sed -e 's/[\/&\\.]/\\\\&/g' | tr \\\\n \\|`
 "$@"
-while inotifywait -qre close_write --exclude '\.(git|idea)\/*' --format "$FORMAT" .
+while inotifywait -qre close_write --exclude "$EXCLUDE" --format "$FORMAT" .
 do
     "$@"
 done
-
