b16a9c007e7224cf2d40f08959e9fd94c71cc808
Markus Tacker <markus@resourceful-humans.com>
2016-12-09 17:58:39 +0100

    Update inotifyrun.sh

--- inotifyrun.sh
+++ inotifyrun.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# You can write a file which contains path names to exclude in the current directory
-# called .inotifyignore
-#
-# It can contain path matchers, one per line, e.g.
-#
-# .git/*
-# tmp/*
-
 while inotifywait -qre close_write --exclude '(\.git/index\.lock|\.idea/.+)' --format "%w%f written" ./
 do
     "$@"
